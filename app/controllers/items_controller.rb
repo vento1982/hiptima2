96786b9eb5fc00d841517d5df5b8173f556feb47
vento <marcin.vento@gmail.com>
2016-10-12 18:29:19 +0200

    Add Completed_at ability

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-	before_action :find_item, only: [ :show, :edit, :update, :destroy ]
+	before_action :find_item, only: [ :show, :edit, :update, :destroy, :complete ]
 	
 	def index 
 		if user_signed_in?
@@ -40,6 +40,11 @@ class ItemsController < ApplicationController
 		redirect_to root_path
 	end
 
+	def complete
+		@item.update_attribute(:completed_at, Time.now)
+		redirect_to root_path
+	end
+	
 	private
 	
 	def item_params
